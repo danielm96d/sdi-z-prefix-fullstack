65ef2ede67b1e4bc5e3a2feeb3dd38580f45a17d
danielm96d <danielthommiller@gmail.com>
2024-08-06 10:46:26 -0600

    finished implementation of the docker-compose file including all three necessary services api, database, and client, verified api and client are live update capable through volumes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import Login from './modules/Login.js'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="*" element={}/>
+        <Route path="*" element={<Login/>}/>
       </Routes>
     </Router>
   );
