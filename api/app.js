3e375977689b1184a5d9f46d6d7d97d112fc09dc
danielm96d <danielthommiller@gmail.com>
2024-08-06 13:15:56 -0600

    finished API implementation of CRUD functionality for the users table

--- api/app.js
+++ api/app.js
@@ -16,6 +16,110 @@ app.get("/", (req, res)=>{
   res.send(`application running using NODE_ENV: ${process.env.NODE_ENV}`);//this line will need editing for deployment
 })
 
+//==================get a list of all inventories====================\\
+app.get("/inventory", async (req, res)=>{
+  try {
+    const adminInventory = await knex("items")
+    .select('*');
+    return res.json(adminInventory)
+  } catch (error) {
+    res.status(500).send('unable to retrieve admin inventory list')
+  }
+})
+
+//==================get a list of a specific user's inventory====================\\
+app.get("/inventory/:id", async (req, res)=>{
+  const {id} = req.params;
+  try {
+    const inventory = await knex("items")
+    .select('*').where({userID: id})
+    return res.json(inventory)
+  } catch (error) {
+    res.status(500).send('unable to retrieve admin inventory list')
+  }
+})
+
+//==================Retrieve all the user information====================\\
+app.get("/users", async (req, res)=>{
+  try {
+    const usersList = await knex("users")
+    .select('*');
+    return res.json(usersList)
+  } catch (error) {
+    res.status(500).send('unable to retrieve admin inventory list')
+  }
+})
+
+//==================Create a User====================\\
+app.post("/users", async (req, res)=>{
+  const userData = req.body
+  console.log(userData)
+  if(!userData)
+    res.status(400).send('empty post request detected please send an appropriate request')
+  try {
+    await knex("users").insert({firstName: userData.firstName, lastName: userData.lastName, userName: userData.userName, password: userData.password})
+    return res.json(userData)
+  } catch (error) {
+    res.status(500).send('unable to retrieve admin inventory list')
+  }
+})
+
+//==================Delete a User====================\\
+app.delete("/users/:id", async (req, res)=>{
+  const {id} = req.params
+  if(!id)
+    res.status(400).send('empty id field request detected please send an appropriate request')
+  try {
+    const deleteUser = await knex("users").where({ id: id }).del()
+    if (deleteUser === 0) {
+      return res.status(404).json({ error: "user not found" });
+    } else {
+      return res.status(202).json("user successfully removed");
+    }
+  } catch (error) {
+    console.log(error)
+    res.status(500).send(error.json)
+  }
+})
+
+//==================Update a User====================\\
+app.patch("/users/:id", async (req, res)=>{
+  const {id} = req.params
+  const {firstName, lastName, userName, password} = req.body
+  if(!id)
+    res.status(400).send('empty id field request detected please send an appropriate request')
+  if(!firstName || !lastName || !userName || !password){
+    res.status(400).send({ERROR: 'please enter an update for each field'})
+  }
+  try {
+    const updateUser = await knex("users")
+      .where({ id: id })
+      .update({firstName, lastName, userName, password})
+    if (updateUser === 0) {
+      return res.status(404).json({ error: "user not found" });
+    } else {
+      return res.status(202).json("user Information Updated");
+    }
+  } catch (error) {
+    console.log(error)
+    res.status(500).send(error.json)
+  }
+})
+
+//==================Retrieve a single users' information====================\\
+app.get("/users/:id", async (req, res)=>{
+  const {id} = req.params;
+  try {
+    const usersList = await knex("users")
+    .select('*').where({id: id});
+    return res.json(usersList)
+  } catch (error) {
+    res.status(500).send('unable to retrieve admin inventory list')
+  }
+})
+
+
+
 app.listen(PORT, () => {
   console.log(`application running using NODE_ENV: ${process.env.NODE_ENV}`);//this line will need editing for deployment
 });
