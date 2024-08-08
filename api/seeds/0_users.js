6de05f8c5207ec03322d563397dd462b43b9fe28
danielm96d <danielthommiller@gmail.com>
2024-08-08 14:31:13 -0600

    updated the readme and user/item seeds

--- api/seeds/0_users.js
+++ api/seeds/0_users.js
@@ -5,14 +5,14 @@
 exports.seed = async function(knex) {
   // Deletes ALL existing entries
   const bcrypt = require('bcryptjs')
-  let pass1 = await bcrypt.hash('1111', 10);
-  let pass2 = await bcrypt.hash('2222', 10);
-  let pass3 = await bcrypt.hash('3333', 10);
+  let pass1 = await bcrypt.hash("smoughs-fat-ass", 10);
+  let pass2 = await bcrypt.hash('jolly-cooperation', 10);
+  let pass3 = await bcrypt.hash('give-it-a-smack', 10);
 
   await knex('users').del()
   await knex('users').insert([
-    {firstname: 'Dan', lastname: 'Miller', username: 'fireball', password: pass1},
-    {firstname: 'Travis', lastname: 'Eltork', username: 'eltork', password: pass2},
-    {firstname: 'Arren', lastname: 'Dedios', username: 'azure', password: pass3}
+    {firstname: 'Leo', lastname: 'Ornstein', username: 'lightning-god69', password: pass1},
+    {firstname: 'Solaire', lastname: 'Astora', username: 'Sun-Man4000', password: pass2},
+    {firstname: 'Alexander', lastname: 'Iron Fist', username: 'Jar-Head-King', password: pass3}
   ]);
 };
--- api/seeds/1_items.js
+++ api/seeds/1_items.js
@@ -6,14 +6,15 @@ exports.seed = async function(knex) {
   // Deletes ALL existing entries
   await knex('items').del()
   await knex('items').insert([
-    {userID: 1, name: 'Couch', description: 'A place for sitting', quantity: 4},
-    {userID: 1, name: 'Trading Cards', description: 'Magic the Gathering Cards', quantity: 10000000},
-    {userID: 1, name: 'Plane', description: 'cesna', quantity: 1},
-    {userID: 2, name: 'Couch', description: 'A place for sitting', quantity: 4},
-    {userID: 2, name: 'Trading Cards', description: 'Magic the Gathering Cards', quantity: 10000000},
-    {userID: 2, name: 'Plane', description: 'cesna', quantity: 1},
-    {userID: 3, name: 'Couch', description: 'A place for sitting', quantity: 4},
-    {userID: 3, name: 'Trading Cards', description: 'Magic the Gathering Cards', quantity: 10000000},
-    {userID: 3, name: 'Plane', description: 'cesna', quantity: 1}
+    {userID: 1, name: 'Excalibur', description: 'Excalibur is the mythical sword of King Arthur that may possess magical powers or be associated with the rightful sovereignty of Britain.', quantity: 4},
+    {userID: 1, name: 'Dragonslayer Spear', description: "Cross spear born from the soul of Ornstein, a Dragonslayer guarding Anor Londo cathedral. Inflicts lightning damage; effective against dragons. Two-handed thrust relies on cross and buries deep within a dragon's hide, and sends human foes flying.", quantity: 2},
+    {userID: 1, name: "Ornstein's Set", description: 'Armor of the dragonslayer Ornstein, who guards the cathedral in the forsaken city of Anor Londo. Ornstein is believed to be the captain of the Four Knights. His golden lion helm is imbued with the power of lightning and should provide good protection against it.', quantity: 1},
+    {userID: 2, name: 'Sunlight Straight Sword', description: "This standard longsword, belonging to Solaire of Astora, is of high quality, is well-forged, and has been kept in good repair. Easy to use and dependable, but unlikely to live up to its grandiose name.", quantity: 1},
+    {userID: 2, name: 'Sunlight Talisman', description: "Medium for casting miracles of the Gods. The talisman of Solaire of Astora, the Knight of Sunlight, is decorated with a holy symbol, illustrated by Solaire himself. This talisman is a projection of Solaire's upstanding, unwavering faith.", quantity: 1},
+    {userID: 2, name: 'Sunlight Shield', description: "Shield of Solaire of Astora, Knight of Sunlight. Decorated with a holy symbol, but Solaire illustrated it himself, and it has no divine powers of its own. As it turns out, Solaire's incredible prowess is a product of his own training, and nothing else.", quantity: 1},
+    {userID: 2, name: 'Iron and Sun Armor', description: "The Armor of Solaire of Astora, Knight of Sunlight, It is Armor of the finest quality and bears the large holy symbol of the sun painted by Solaire himself, This armor of high quality but lacks any particular power", quantity: 1},
+    {userID: 3, name: 'Shard of Alexander', description: "Shard of the late Alexander, a shattered warrior jar.", quantity: 10},
+    {userID: 3, name: "Alexander's Innards", description: "A keepsake of the warrior jar Alexander. Found at the core of the dead flesh that once filled the great jar. The jars contain dregs inherited from those who came before. Thus are warriors passed from jar to jar, carrying dreams of greatness.", quantity: 1},
+    {userID: 3, name: 'HEART', description: "Alexander gained the heart of all through his journey's and is cherished by too many to count", quantity: 999999999}
   ]);
 };
